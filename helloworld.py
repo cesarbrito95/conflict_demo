77a2214c335ee0652e9a289a0bb600472f8ea4d4
Julio Brito <cesarbrito999@gmail.com>
2022-04-29 16:48:26 -0500

    merge

--- helloworld.py
+++ helloworld.py
@@ -1,26 +1,9 @@
-<< << << < HEAD
-
-
 def hello_world(city, state):
     print(f'Hello World {city}, {state}!')
 
 
 def main():
     hello_world('NYC', 'New York')
-
-
-== == == =
-
-
-def hello_world(city):
-    print(f'Hello World {city}!')
-
-
-def main():
-    hello_world('NYC')
-
-
->>>>>> > e88769bc9c176cc6416c5cb1c6904255c083ae11
 
 
 main()
